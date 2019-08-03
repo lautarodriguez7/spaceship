2f3dee84c38d68c81ec5a26960ac1fb6e5768301
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-03 14:15:37 -0300

    new variables

--- js/game.js
+++ js/game.js
@@ -1,15 +1,29 @@
 (function(){
     'use strict';
-var pressing = [];
+    window.addEventListener('load', init, false);
+    var KEY_ENTER = 13,
+        KEY_LEFT = 37,
+        KEY_UP = 38,
+        KEY_RIGHT = 39,
+        KEY_DOWN = 40;
 
-document.addEventListener('keydown', function(evt) {
-    lastPress = evt.keyCode;
-    pressing[evt.keyCode] = true;
-}, false)
+    var canvas = null,
+        ctx = null,
+        x = 50,
+        y = 50,
+        lastPress = null,
+        pressing = [],
+        pause = true;
 
-document.addEventListener('keyup', function(evt) {
-    lastPress = evt.keyCode;
-    pressing[evt.keyCode] = true;
-}, false)
 
-})();
+    document.addEventListener('keydown', function(evt) {
+        lastPress = evt.keyCode;
+        pressing[evt.keyCode] = true;
+    }, false)
+
+    document.addEventListener('keyup', function(evt) {
+        lastPress = evt.keyCode;
+        pressing[evt.keyCode] = true;
+    }, false)
+
+    })();
