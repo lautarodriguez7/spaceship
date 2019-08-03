a2e8d187ea154f2e8b8b0d8a5cb085479e0983d4
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-03 14:29:17 -0300

    new function "paint()"

--- js/game.js
+++ js/game.js
@@ -28,6 +28,52 @@
         parseInt(ctx);
     }
 
+    function act() {
+        if (!pause) {
+        // Move rect
+        if (pressing[KEY_UP])
+            y -= 10;
+        if (pressing[KEY_RIGHT])
+            x += 10;
+        if (pressing[KEY_DOWN])
+            y += 10;
+        if (pressing[KEY_LEFT])
+            x -= 10;
+
+        // Out screen
+        if (x > canvas.width)
+            x = 0;
+        if (y > canvas.height)
+            y = 0;
+        if (x < 0)
+            x = canvas.width;
+        if (y < 0)
+            y = canvas.height;
+        }
+        // Pause/Unpause
+        if (lastPress == KEY_ENTER) {
+            pause = !pause;
+            lastPress = null;
+        }
+    }
+
+    function paint(ctx) {
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#0f0';
+        ctx.fillRect(x, y, 10, 10);
+
+        ctx.fillStyle = '#fff';
+        //ctx.fillText('Last Press: '+lastPress,0,20);
+
+        if (pause) {
+            ctx.textAlign = 'center';
+            ctx.fillText('PAUSE', 150, 75);
+            ctx.textAlign='left';
+        }
+    }
+
     document.addEventListener('keydown', function(evt) {
         lastPress = evt.keyCode;
         pressing[evt.keyCode] = true;
