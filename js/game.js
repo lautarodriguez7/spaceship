38c6d90d6c921c4337213b2704654c3cd72b1c72
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-08-03 14:06:57 -0300

    we transform into local function

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
-var pressing = [];
+(function(){
+    'use strict';
 
 document.addEventListener('keydown', functio(evt) {
     lastPress = evt.keyCode;
     pressing[evt.keyCode] = true;
 }, false)
+    
+})();
